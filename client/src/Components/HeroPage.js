b05bcf427ac7c24559bb4a97bc75804ee458e3f3
Rakesh Rajpurohit <rakesh.rajpurohit.rr.rr.201@gmail.com>
2021-06-30 18:06:01 +0000

    Hero Page completed!

---
+++ client/src/Components/HeroPage.js
@@ -0,0 +1,21 @@
+import React from "react";
+import home from "../assets/home.svg";
+
+const HeroPage = () => {
+  return (
+    <div className="d-flex flex-direction-row justify-content-between align-items-center">
+      <div className="">
+        <h1>Start conversation</h1>
+        <p className="lead text-md-start text-center mb-5">
+          We help people accomplish their goals!
+        </p>
+        <button className="btn btn-primary">Learn More</button>
+      </div>
+      <div className="d-none d-md-block">
+        <img src={home} className="image" />
+      </div>
+    </div>
+  );
+};
+
+export default HeroPage;
--- client/src/Screens/Home.js
+++ client/src/Screens/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
+import HeroPage from "../Components/HeroPage";
 import Navbar from "../Components/Navbar";
-import home from "../assets/home.svg";
 
 const Home = () => {
   return (
@@ -9,18 +9,7 @@ const Home = () => {
         <Navbar />
       </section>
       <section className="container p-5">
-        <div className="d-flex flex-direction-row justify-content-between align-items-center">
-          <div className="">
-            <h1>Start conversation</h1>
-            <p className="lead text-md-start text-center mb-5">
-              We help people accomplish their goals!
-            </p>
-            <button className="btn btn-primary">Learn More</button>
-          </div>
-          <div className="d-none d-md-block">
-            <img src={home} class="image" />
-          </div>
-        </div>
+        <HeroPage />
       </section>
     </div>
   );
