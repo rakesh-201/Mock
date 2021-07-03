76dfeaea571160c1579f3e3be70387fa8398c3f3
Rakesh Rajpurohit <rakesh.rajpurohit.rr.rr.201@gmail.com>
2021-07-03 16:30:41 +0000

    some styling of room card is remaining.

---
+++ client/src/Components/RoomCard.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../Styles/RoomCard.css";
+
+const RoomCard = () => {
+  return (
+    <div className="cont">
+      <div className="color-cont">
+        <div className="content-cont ">The title</div>
+      </div>
+    </div>
+  );
+};
+
+export default RoomCard;
--- client/src/Screens/HomeIn.js
+++ client/src/Screens/HomeIn.js
@@ -1,9 +1,14 @@
 import React from "react";
+import RoomCard from "../Components/RoomCard";
 
 const HomeIn = () => {
   return (
-    <div>
-      {/* <Navbar /> */}
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-3 col-md-4 col-12">
+          <RoomCard />
+        </div>
+      </div>
     </div>
   );
 };
