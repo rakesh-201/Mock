b08fa86518df470a6f54fd0ff4d967132a338cdf
Rakesh Rajpurohit <rakesh.rajpurohit.rr.rr.201@gmail.com>
2021-07-04 18:34:51 +0000

    Room Card nearly completed.

--- client/src/Components/RoomCard.js
+++ client/src/Components/RoomCard.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "../Styles/RoomCard.css";
 
-const RoomCard = () => {
+const RoomCard = ({ heading, text }) => {
   return (
-    <div className="cont">
-      <div className="color-cont">
-        <div className="content-cont ">The title</div>
+    <div className="cont shadow">
+      <div className="cont2">
+        <div className="bg-primary" id="rod1"></div>
+        <div className="content-cont ">
+          <p className="fw-bold">{heading}</p>
+          {text.substr(0, 40) + "..."}
+        </div>
       </div>
     </div>
   );
